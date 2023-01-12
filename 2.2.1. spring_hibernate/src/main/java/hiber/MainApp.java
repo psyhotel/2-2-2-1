d7cc094cb22740bfe2e2f1fcde8e624305746d3c
psyhotel <sative2010@yandex.ru>
2023-01-12 18:07:46 +0500

    BUILD SUCCESS

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,11 +16,6 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-//        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-//        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-//        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-//        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-
         Car MercedesBenz = new Car();
         MercedesBenz.setModel("S");
         MercedesBenz.setSeries(600);
